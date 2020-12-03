e5a77fb5e0e7ea70b0c7dc0aad4bee6dd98c9665
Romain Lanz <romain.lanz+github@pm.me>
2020-12-03 22:10:35 +0100

    feat: learn to do css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,25 @@
 <template>
-	<div class="flex items-center justify-between h-16 px-8 text-2xl text-white border-b-4 bg-bg border-follow">
+	<div class="grid h-16 grid-cols-3 px-8 text-2xl text-white border-b-4 bg-bg border-follow">
 		<div>
 			<!-- Songs -->
 		</div>
 
-		<div class="flex">
-			<div class="h-8 ml-4 overflow-hidden bg-white rounded-full" style="width: 500px">
+		<div class="flex items-center justify-center">
+			<div class="w-full h-8 overflow-hidden bg-white rounded-full">
 				<div
 					class="flex items-center justify-center h-full font-medium bg-follow"
 					:style="{
 						width: `${(subscribers / subGoal) * 100}%`,
 					}"
 				>
-					<span>{{ subscribers }} / {{ subGoal }}</span>
+					<span>{{ subscribers }} subs / {{ subGoal }}</span>
 				</div>
 			</div>
 		</div>
-		<span>{{ viewers }} viewers | {{ followers }} followers</span>
+
+		<div class="flex items-center justify-end">
+			<span>{{ viewers }} viewers | {{ followers }} followers</span>
+		</div>
 	</div>
 </template>
 
