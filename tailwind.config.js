fafd89fa4630536270d20d0656a6b9409eae81d2
Romain Lanz <romain.lanz+github@pm.me>
2020-12-14 21:10:49 +0100

    chore: purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		// removeDeprecatedGapUtilities: true,
 		// purgeLayersByDefault: true,
 	},
-	purge: [],
+	purge: ['./src/**/*.vue'],
 	theme: {
 		extend: {
 			fontFamily: {
