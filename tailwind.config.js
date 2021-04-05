99caf86c08aa14c8253a7423a12f87794ea38d1b
Romain Lanz <romain.lanz+github@pm.me>
2021-04-05 20:16:22 +0200

    chore: update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Work Sans'],
+				base: ['Cascadia Code'],
+			},
+			colors: {
+				follow: '#fb6745',
+				host: '#e8368f',
+				raid: '#ef4e4e',
+				sub: '#5a45ff',
+				bg: '#212935',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
