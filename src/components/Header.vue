5785951f4b9053cb0356fcbde2c4b2115aded5e5
Romain Lanz <romain.lanz+github@pm.me>
2021-11-01 19:44:24 +0100

    feat: update colors

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="grid h-16 grid-cols-2 px-8 text-2xl text-white border-b-4 bg-bg border-follow">
+	<div class="relative grid h-16 grid-cols-2 px-8 overflow-hidden text-2xl text-white border-b-4 bg-bg border-purple">
+		<div class="radient"></div>
+
 		<div>
 			<!-- Songs -->
 		</div>
@@ -17,3 +19,15 @@
 	const followers = useFollowerCount();
 	const viewers = useViewerCount();
 </script>
+
+<style>
+	.radient {
+		position: absolute;
+		top: -125px;
+		left: -20px;
+		background: linear-gradient(#00ffff, #e500f1);
+		height: 250px;
+		width: 1200px;
+		filter: blur(30px);
+	}
+</style>
